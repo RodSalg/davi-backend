613edd05a1b9b930eadf9ca63a5ffb750fc3bf34
Thiago Salgado <thiago.salgado@fit-tecnologia.org.br>
2024-12-02 09:19:16 -0400

    swagger update

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ async function bootstrap() {
   .setVersion('0.1')
   .build();
 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3337);
 }
 
